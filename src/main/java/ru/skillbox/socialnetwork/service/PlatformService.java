94ea52c1d8854ebd417bd9ab1136dcf319a89ba7
Platonov Yury <platonov230388@gmail.com>
2021-09-27 18:14:01 +0300

    s1/t17 added Platform GET /countries /cities

--- src/main/java/ru/skillbox/socialnetwork/service/PlatformService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/PlatformService.java
@@ -1,7 +1,11 @@
 package ru.skillbox.socialnetwork.service;
 
-import ru.skillbox.socialnetwork.data.dto.LanguageResponse;
+import ru.skillbox.socialnetwork.data.dto.PlatformResponse;
 
 public interface PlatformService {
-    LanguageResponse getLanguage();
+    PlatformResponse getLanguage();
+
+    PlatformResponse getCountries(String country, int offset, int itemPerPage);
+
+    PlatformResponse getCities(Long countryId, String city, int offset, int itemPerPage);
 }
