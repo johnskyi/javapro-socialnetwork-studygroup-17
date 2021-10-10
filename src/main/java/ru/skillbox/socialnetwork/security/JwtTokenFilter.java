4d26c3fea1d220e28b585de387b0bcbf9b0166c8
An4oy3 <sinevichaleksandr@gmail.com>
2021-10-10 21:38:43 +0300

    Add JWTTokens in Security. Change ResponseBody in login()

---
+++ src/main/java/ru/skillbox/socialnetwork/security/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package ru.skillbox.socialnetwork.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import ru.skillbox.socialnetwork.exception.JwtAuthenticationException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtTokenFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                if (authentication != null) {
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }catch (JwtAuthenticationException e){
+            SecurityContextHolder.clearContext();
+            ((HttpServletResponse) servletResponse).sendError(e.getHttpStatus().value());
+            throw new JwtAuthenticationException("JWT token invalid");
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
---
+++ src/main/java/ru/skillbox/socialnetwork/security/JwtTokenProvider.java
@@ -0,0 +1,69 @@
+package ru.skillbox.socialnetwork.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import ru.skillbox.socialnetwork.exception.JwtAuthenticationException;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final UserDetailsService userDetailsService;
+    private String secretKey = "javaPro";
+    private final String authorizationHeader = "Authorization";
+
+    public JwtTokenProvider(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String creteToken(String username){
+        Claims claims = Jwts.claims().setSubject(username);
+        Date now = new Date();
+        long validityInMilliseconds = 1800000;
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claimsJws.getBody().getExpiration().before(new Date());
+        }catch (JwtException | IllegalArgumentException exception){
+            throw new JwtAuthenticationException("JWT token invalid", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+    public String getUsername(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader(authorizationHeader);
+    }
+}
