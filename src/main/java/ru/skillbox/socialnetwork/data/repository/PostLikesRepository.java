5b128f020bae69aa1fd80dbc4d4da178b71b2e94
Alexey Kochenyuk <alexey503@gmail.com>
2021-10-17 23:37:44 +0300

    likes doing

--- src/main/java/ru/skillbox/socialnetwork/data/repository/PostLikesRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/data/repository/PostLikesRepository.java
@@ -5,9 +5,19 @@ import ru.skillbox.socialnetwork.data.entity.Person;
 import ru.skillbox.socialnetwork.data.entity.Post;
 import ru.skillbox.socialnetwork.data.entity.PostLike;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostLikesRepository extends JpaRepository<PostLike, Long> {
 
     Integer countByPost(Post post);
 
     Integer countByPerson(Person person);
+
+    Optional<PostLike> findByPersonIdAndPostId(long personId, long postId);
+
+    Optional<PostLike> findByPerson(Person person);
+
+
+    List<Long>findAllPersonIdByPostId(long postId);
 }
---
+++ src/main/java/ru/skillbox/socialnetwork/service/LikesService.java
@@ -0,0 +1,94 @@
+package ru.skillbox.socialnetwork.service;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.skillbox.socialnetwork.data.dto.Likes.LikedResponse;
+import ru.skillbox.socialnetwork.data.dto.Likes.LikeUsersListResponse;
+import ru.skillbox.socialnetwork.data.dto.Likes.LikesCountResponse;
+import ru.skillbox.socialnetwork.data.entity.Person;
+import ru.skillbox.socialnetwork.data.entity.Post;
+import ru.skillbox.socialnetwork.data.entity.PostLike;
+import ru.skillbox.socialnetwork.data.repository.*;
+
+import javax.swing.text.html.Option;
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LikesService {
+/*
+
+    private final PostRepository postRepository;
+    private final TagRepository tagRepository;
+    private final Post2TagRepository post2TagRepository;
+    private final PostCommentsRepository postCommentsRepository;
+
+ */
+    private final PersonRepo personRepository;
+    private final PostLikesRepository postLikesRepository;
+    private final PostRepository postRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(LikesService.class);
+
+    public LikedResponse isLikedByPerson(Long personId, Long itemId, String type) {
+        return LikedResponse.builder()
+                .error("string")
+                .timestamp(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+                .data(LikedResponse.Data.builder()
+                        .likes(postLikesRepository.findByPersonIdAndPostId(personId, itemId).isPresent())
+                        .build())
+                .build();
+    }
+
+    public LikeUsersListResponse getLikeUsersList(Long itemId, String type) {
+
+        List<Long> likesUsersList = postLikesRepository.findAllPersonIdByPostId(itemId);
+
+        return LikeUsersListResponse.builder()
+                .error("string")
+                .timestamp(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+                .data(LikeUsersListResponse.Data.builder()
+                        .likes(likesUsersList.size())
+                        .users(likesUsersList)
+                        .build())
+                .build();
+    }
+
+    public LikeUsersListResponse putLike(long itemId, String type, Principal principal) {
+        Person author = personRepository.findByEmail(principal.getName())
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        Post post = postRepository.getById(itemId);
+
+        postLikesRepository.save(PostLike.builder()
+                .time(LocalDateTime.now(ZoneOffset.UTC))
+                .person(author)
+                .post(post)
+                .build());
+
+        return getLikeUsersList(itemId, type);
+    }
+
+    public LikesCountResponse deleteLike(Long itemId, String type, Principal principal) {
+        Post post = postRepository.getById(itemId);
+
+        Person author = personRepository.findByEmail(principal.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        postLikesRepository.findByPerson(author).ifPresent(postLikesRepository::delete);
+
+        return LikesCountResponse.builder()
+                .error("string")
+                .timestamp(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+                .data(LikesCountResponse.Data.builder()
+                        .likes(postLikesRepository.countByPost(post))
+                        .build())
+                .build();
+    }
+}
