0d812e2cab4d4b5b70428778becbc9a7b1787370
Semin Evgeniy <johnskyi2@yandex.ru>
2021-12-06 19:52:38 +0300

    added test methods DialogControllerImpl.java

--- src/main/java/ru/skillbox/socialnetwork/controller/DialogController.java
+++ src/main/java/ru/skillbox/socialnetwork/controller/DialogController.java
@@ -11,23 +11,23 @@ public interface DialogController {
 
     /**Отправка сообщений.*/
     @PostMapping("/api/v1/dialogs/messages")
-    ResponseEntity<DialogResponse> sendMessage(@RequestBody DialogRequest request);
+    ResponseEntity<DialogResponse> sendMessage(@RequestBody DialogRequest request, Principal principal);
     /**Получить список сообщений в диалоге.*/
     @GetMapping("/api/v1/dialogs/{id}/messages")
     ResponseEntity<DialogResponse> getAllMessages(@PathVariable Long dialogId);
 
     /**Создать диалог.*/
     @PostMapping("/api/v1/dialogs")
-    ResponseEntity<DialogResponse> dialogCreate(@RequestParam("userId") Long userId);
+    ResponseEntity<DialogResponse> dialogCreate(@RequestParam("userId") Long userId,Principal principal);
 
     /**Delete dialog by Id.*/
     @DeleteMapping("/api/v1/dialogs/")
-    ResponseEntity<DialogResponse> dialogDelete(@RequestParam("dialogId") Long dialogId);
+    ResponseEntity<DialogResponse> dialogDelete(@RequestParam("dialogId") Long dialogId, Principal principal);
 
     /**Delete message by Id.*/
     @DeleteMapping("/api/v1/dialogs/{dialogId}/messages/")
     ResponseEntity<DialogResponse> messageDelete(@PathVariable Long dialogId,
-                                                 @RequestParam("messageId") Long messageId);
+                                                 @RequestParam("messageId") Long messageId, Principal principal);
 
 
 }
